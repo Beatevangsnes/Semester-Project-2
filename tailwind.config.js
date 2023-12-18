805f0f66efef0c01f951251c210a2c3fd299fc9b
Beatevangsnes <112164771+Beatevangsnes@users.noreply.github.com>
2023-12-18 02:20:26 +0100

    css and tailwind config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./index.html",
-    "./node_modules/flowbite/**/*.js",
+    "./*.html", // Include HTML files in the root directory
+    "./*.css", // Include CSS files in the root directory
+    "./src/**/*.mjs", // Include .mjs files in the src directory and its subdirectories
+    "./listings/**/*.html", // Include HTML files in the listings directory and its subdirectories
+    "./listing/**/*.html", // Include HTML files in the listing directory and its subdirectories
+    "./profile/**/*.html", // Include HTML files in the profile directory and its subdirectories
   ],
   theme: {
     extend: {},
