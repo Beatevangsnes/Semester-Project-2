8a0182a2a9bd1ae2f3d7d3cddb5b7e97691185d2
Beatevangsnes <112164771+Beatevangsnes@users.noreply.github.com>
2023-12-18 03:46:26 +0100

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html", // Include HTML files in the root directory
-    "./*.css", // Include CSS files in the root directory
-    "./src/**/*.mjs", // Include .mjs files in the src directory and its subdirectories
-    "./listings/**/*.html", // Include HTML files in the listings directory and its subdirectories
-    "./listing/**/*.html", // Include HTML files in the listing directory and its subdirectories
-    "./profile/**/*.html", // Include HTML files in the profile directory and its subdirectories
+    "./*.html", // Root HTML files
+    "./dist/css/*.css", // CSS files in dist/css
+    "./src/**/*.{html,mjs}", // HTML and .mjs files in src and subdirectories
+    "./listings/**/*.{html,mjs}", // listings directory
+    "./listing/**/*.{html,mjs}", // listings directory
+    "./profile/**/*.{html,mjs}", // profile directory
+    // Add other directories as needed
   ],
   theme: {
     extend: {},
